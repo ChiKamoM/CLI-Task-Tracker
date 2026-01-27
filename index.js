8d923de9a845f571291325349565127c128a2e93
Chinasa <kamogelomba@gmail.com>
2026-01-27 15:56:41 +0200

    added more functions

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-// Add, Update, and Delete tasks
+// Add, Update, and Delete tasks X
 
-// Mark a task as in progress or done
+// Mark a task as in progress or done X
 
 // List all tasks
 
@@ -10,25 +10,65 @@
 
 // List all tasks that are in progress
 
+function wait(ms){
+   var start = new Date().getTime();
+   var end = start;
+   while(end < start + ms) {
+     end = new Date().getTime();
+  }
+}
+
 const tasks = [
-    {id:1,
-    description:'Buy groceries',
-    status:'in-progress',
-    createedAt:'',
-    updatedAt:''},
+  {
+    id: 0,
+    description: "Buy groceries",
+    status: "in-progress",
+    createdAt: now,
+    updatedAt: ""
+  },
+  {
+    id: 1,
+    description: "Finish homework",
+    status: "todo",
+    createdAt: now,
+    updatedAt: ""
+  },
+  {
+    id: 2,
+    description: "Clean bedroom",
+    status: "done",
+    createdAt: now,
+    updatedAt: now
+  },
+  {
+    id: 3,
+    description: "Read JavaScript chapter",
+    status: "in-progress",
+    createdAt: now,
+    updatedAt: ""
+  },
+  {
+    id: 4,
+    description: "Hand in homework",
+    status: "done",
+    createdAt: now,
+    updatedAt: now
+  }
 ]
 
-const now = new Date()
-const statuses = ['todo', 'in-progress', 'complete']
+let now = new Date()
+const statuses = ['todo', 'in-progress', 'done']
+
+const  exampleTask = "Finish chores"
+const editedTask = "Hand in chores"
 
 function nextID(array){
     const index = Number(array.length - 1)
     return array[index].id + 1
 }
 
-const  exampleTask = "Finish homework"
-
 function newTask(task){
+    console.log(tasks)
     tasks.push({
         id:nextID(tasks),
         description:task,
@@ -38,15 +78,66 @@ function newTask(task){
     })
 }
 
-function updateTask(id,time) {
-    
-    tasks.indexOf()
-    // selectedTask.updatedAt = now
-    console.log(selectedTask.id)
-    console.log(`The following task was selected ${selectedTask}`)
-    
+function updateTask(id,newTask) {    
+    if(tasks.find(task => task.id == id)){
+    const selectedTask= tasks.find(task => task.id = id)
+    const taskID = selectedTask.id
+    tasks[taskID].description = newTask
+    tasks[taskID].updatedAt = new Date()
+    console.log(selectedTask)
+
+}else{
+        console.log(`No task with id ${id}`)
+    }    
 }
+
+function updateStatus(id,status){
+    if(tasks.find(task => task.id == id)){
+        const selectedTask= tasks.find(task => task.id = id)
+        const taskID = selectedTask.id
+        tasks[taskID].status = statuses[status]
+        tasks[taskID].updatedAt = new Date()
+        console.log(selectedTask)
+
+}else{
+        console.log(`No task with id ${id}`)
+    }
+}
+
+function listTasks(){
+    console.log(tasks)
+}
+
+function listIncompleteTasks(){
+    let incomplete = []
+    tasks.forEach(task => {
+        task.status =='to-do' || task.status ==  'in-progress' ? incomplete.push(push):null
+    });
+    if(incomplete.length>0){
+        console.log(inProgress)
+    }
+}
+
+function listCompletTasks(){
+    let complete = []
+    tasks.forEach(task => {
+        task.status =='done' ? complete.push(push):null
+    });
+    if(complete.length>0){
+        console.log(inProgress)
+    }
+}
+
+function listInProgressTasks(){
+    let inProgress = []
+    tasks.forEach(task => {
+        task.status =='in-progress' ? incomplete.push(push):null
+    });
+    if(inProgress.length>0){
+        console.log(inProgress)
+    }
+}
+
 newTask(exampleTask)
-updateTask(2)
-console.log(tasks )
-//array.findS
+console.log(tasks)
+
